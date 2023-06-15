9a8692b438a390cd555e8988132f26a5ae1bbe67
TechWithTyler <twsandmds@gmail.com>
2023-06-15 13:27:13 -0700

    Initial Commit

---
+++ Phone Booth/Phone_BoothApp.swift
@@ -0,0 +1,20 @@
+//
+//  Phone_BoothApp.swift
+//  Phone Booth
+//
+//  Created by Tyler Sheft on 6/15/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Phone_BoothApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(for: Item.self)
+    }
+}
