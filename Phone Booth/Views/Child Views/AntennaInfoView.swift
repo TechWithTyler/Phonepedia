971b7c31ba0df45a70131b4a5787a998205f22ac
TechWithTyler <twsandmds@gmail.com>
2023-10-11 12:42:13 -0700

    Added more fields and descriptions.

---
+++ Phone Booth/Views/Child Views/AntennaInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  AntennaInfoView.swift
+//  Phone Booth
+//
+//  Created by Tyler Sheft on 10/11/23.
+//
+
+import SwiftUI
+
+struct AntennaInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AntennaInfoView()
+}
