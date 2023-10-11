15d3f88cb284fe9b98061c3f2a8c37854c613b8c
TechWithTyler <twsandmds@gmail.com>
2023-10-11 13:15:52 -0700

    Updated descriptions

--- Phone Booth/Views/Detail/PhoneDetailView.swift
+++ Phone Booth/Views/Detail/PhoneDetailView.swift
@@ -86,11 +86,24 @@ struct PhoneDetailView: View {
 								Text("5.8GHz/2.4GHz Digital Frequency-Hopping Spread Spectrum (FHSS)").tag(22)
 							}
 							Section(header: Text("DECT")) {
-								Text("DECT (1.88-1.90GHz)").tag(23)
-								Text("DECT (1.90-1.92GHz)").tag(24)
-								Text("DECT 6.0 (1.92-1.93GHz)").tag(25)
+								Text("DECT (1.88GHz-1.90GHz)").tag(23)
+								Text("DECT (1.90GHz-1.92GHz)").tag(24)
+								Text("DECT 6.0 (1.92GHz-1.93GHz)").tag(25)
 							}
 						}
+						HStack {
+							Image(systemName: "info.circle")
+							Text("""
+   A lower frequency means more range.
+   Analog phones are prone to interference and their signals can be picked up by anyone using a radio scanner set to the same frequency, which isn't ideal for private conversations.
+   Voice scramble phones scramble the audio before it's transmitted between the base and handset, and is then unscrambled when it's sent to the receiving end. Example: When you say "Hello" into the handset, it becomes "Grblm" before it is sent to the base, and then becomes "Hello" again before it's sent to the caller. This way, anyone using a radio scanner to pick up the signal will hear the scrambled audio, which they won't be able to understand.
+   "Frequency 1/Frequency 2" (e.g. 5.8GHz/900MHz) means that the base transmits to the handset using frequency 1 and the handset transmits to the base using frequency 2. As a result, the phone may lose connection on the transmit side before losing connection on the receiving side, or vice versa.
+   "Frequency 1-Frequency 2" (e.g. 1.92GHz-1.93GHz) means that the phone operates in a range of frequencies, not just one specific frequency.
+   DECT uses encryption, making it the most secure cordless phone frequency. And since it's dedicated to cordless phones, baby monitors, and related devices, other wireless devices won't interfere with the phone.
+   """)
+						}
+						.font(.footnote)
+						.foregroundStyle(.secondary)
 						Picker("Antennas", selection: $phone.antennas) {
 							Text("Hidden").tag(0)
 							Text("Telescopic").tag(1)
