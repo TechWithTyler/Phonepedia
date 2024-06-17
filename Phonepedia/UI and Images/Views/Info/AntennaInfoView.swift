08741121d16906e8403ce253f71b81607600beed
TechWithTyler <twsandmds@gmail.com>
2024-06-17 13:14:49 -0700

    Increase min OS version
    Due to macOS 14 Sonoma having a critical bug where quitting the app after changing a phone's color fields would cause a crash, Phonepedia will now require macOS 15 Sequoia/iOS 18/visionOS 2 or later.

--- Phonepedia/UI and Images/Views/Info/AntennaInfoView.swift
+++ Phonepedia/UI and Images/Views/Info/AntennaInfoView.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import SheftAppsStylishUI
 
 struct AntennaInfoView: View {
     
--- Phonepedia/UI and Images/Views/Info/BatteryInfoView.swift
+++ Phonepedia/UI and Images/Views/Info/BatteryInfoView.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import SheftAppsStylishUI
 
 struct BatteryInfoView: View {
     var body: some View {
