5c54ea4d0ddf8791dff63fa7f0ca554d6cd1a199
TechWithTyler <twsandmds@gmail.com>
2024-06-27 13:20:44 -0700

    Added SwiftUI preview to HandsetInfoDetailView

--- Phonepedia/UI and Images/Views/Detail/HandsetInfoDetailView.swift
+++ Phonepedia/UI and Images/Views/Detail/HandsetInfoDetailView.swift
@@ -405,8 +405,8 @@ struct HandsetInfoDetailView: View {
 
 }
 
-//#Preview {
-//	@State var handset = CordlessHandset(brand: "Panasonic", model: "KX-TGFA97")
-//	handset.phone = Phone(brand: "Panasonic", model: "KX-TGF975")
-//	return HandsetInfoDetailView(handset: $handset, handsetNumber: 1)
-//}
+#Preview {
+    @State var handset = CordlessHandset(brand: "Panasonic", model: "KX-TGFA97", mainColorRed: 120, mainColorGreen: 120, mainColorBlue: 120, secondaryColorRed: 0, secondaryColorGreen: 0, secondaryColorBlue: 0)
+	handset.phone = Phone(brand: "Panasonic", model: "KX-TGF975")
+    return HandsetInfoDetailView(handset: $handset, handsetNumber: 1)
+}
