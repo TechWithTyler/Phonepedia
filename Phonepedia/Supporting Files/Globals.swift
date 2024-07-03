2d77bc1d74f3ed7eced4e0df78e20b86bb0f426b
TechWithTyler <twsandmds@gmail.com>
2024-07-03 13:57:11 -0700

    Update Globals.swift
    Refactored code and added comments

--- Phonepedia/Supporting Files/Globals.swift
+++ Phonepedia/Supporting Files/Globals.swift
@@ -12,14 +12,20 @@ import SwiftUI
 // Names to use for the answering system greeting/talking caller ID examples
 var names: [String] = ["John Smith", "Pat Fleet", "Allison Smith", "Charlie Johnson"]
 
+// Takes the given name and converts it from "First Last" to "LAST, FIRST".
 func cnamForName(_ name: String) -> String {
+    // 1. Split name into separate components.
     let splitName = name.components(separatedBy: " ")
+    // 2. Get the first and last names and convert them to uppercase.
     let firstName = splitName.first?.uppercased()
     let lastName = splitName.last?.uppercased()
-    return "\(lastName!), \(firstName!)"
+    // 3. Create a new String with the format "LAST, FIRST".
+    let lastFirstName = "\(lastName!), \(firstName!)"
+    // 4. Return the new String.
+    return lastFirstName
 }
 
-// The current year to use as the default value of a phone or handset's release year.
+// The current year to use as the default value of a phone or handset's release year/acquisition year.
 var currentYear = Calendar.current.component(.year, from: Date())
 
 #if os(macOS)
