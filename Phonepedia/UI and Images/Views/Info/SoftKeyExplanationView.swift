8d55eeaa8f3118222d63b557f21c21212495171f
TechWithTyler <twsandmds@gmail.com>
2024-07-24 11:06:12 -0700

    Update SoftKeyExplanationView.swift
    Reverted Dynamic Type change as it wasn't working.

--- Phonepedia/UI and Images/Views/Info/SoftKeyExplanationView.swift
+++ Phonepedia/UI and Images/Views/Info/SoftKeyExplanationView.swift
@@ -104,7 +104,6 @@ struct SoftKeyExplanationView: View {
                     .accessibilityElement(children: .ignore)
                     .accessibilityLabel(softKeyAccessibilityDescription(placement: .left, marking: .dash, textCase: .capitalized, hasTextBackground: false))
                 }
-                .dynamicTypeSize(.medium)
             }
             Spacer()
         }
