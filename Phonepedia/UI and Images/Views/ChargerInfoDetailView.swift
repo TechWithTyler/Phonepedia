8e882c1ba24988c28911537f4f56479ca3c5fcc4
TechWithTyler <twsandmds@gmail.com>
2024-10-03 14:23:33 -0700

    Organized files

--- Phonepedia/UI and Images/Views/ChargerInfoDetailView.swift
+++ Phonepedia/UI and Images/Views/ChargerInfoDetailView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 import SheftAppsStylishUI
 
 struct ChargerInfoDetailView: View {
-    
+
     // MARK: - Properties - Charger
 
-	@Bindable var charger: CordlessHandsetCharger
+    @Bindable var charger: CordlessHandsetCharger
 
     // MARK: - Properties - Dismiss Action
 
@@ -29,9 +29,9 @@ struct ChargerInfoDetailView: View {
 
     // MARK: - Body
 
-	var body: some View {
-		if let phone = charger.phone {
-			Form {
+    var body: some View {
+        if let phone = charger.phone {
+            Form {
                 Section {
                     Button {
                         phone.chargersIHave.insert(charger.duplicate(), at: chargerNumber)
@@ -50,10 +50,10 @@ struct ChargerInfoDetailView: View {
 #endif
                     }
                     ColorPicker("Main Color", selection: charger.mainColorBinding)
-                        ColorPicker("Secondary/Accent Color", selection: charger.secondaryColorBinding)
-                        Button("Use Main Color") {
-                            charger.setSecondaryColorToMain()
-                        }
+                    ColorPicker("Secondary/Accent Color", selection: charger.secondaryColorBinding)
+                    Button("Use Main Color") {
+                        charger.setSecondaryColorToMain()
+                    }
                     Picker("Charging Direction", selection: $charger.chargingDirection) {
                         Text("Forward (stand up)").tag(0)
                         Text("Forward (lean back)").tag(1)
@@ -81,20 +81,17 @@ struct ChargerInfoDetailView: View {
                         Text("Bracket").tag(2)
                     }
                 }
-                Section("Special Features") {
-					if phone.supportsRangeExtenders {
-						Toggle("Has Range Extender", isOn: $charger.hasRangeExtender)
-                        InfoText("A charger with a built-in range extender allows you to have a range extender where you have a charger, without having to register and place a separate range extender.")
-					}
-                    
-				}
-			}
-			.formStyle(.grouped)
-		} else {
-			Text("Error")
-		}
-	}
-    
+                if phone.supportsRangeExtenders {
+                    Toggle("Has Range Extender", isOn: $charger.hasRangeExtender)
+                    InfoText("A charger with a built-in range extender allows you to have a range extender where you have a charger, without having to register and place a separate range extender.")
+                }
+            }
+            .formStyle(.grouped)
+        } else {
+            Text("Error")
+        }
+    }
+
 }
 
 #Preview {
