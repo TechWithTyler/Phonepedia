3eae6250c6b3cf2ba3bf78ddbc0f023d2d085620
TechWithTyler <twsandmds@gmail.com>
2025-01-07 13:38:29 -0800

    Removed phone image unavailable view as it wasn't needed

--- Phonepedia/UI and Images/Views/Child Views/PhoneImage.swift
+++ Phonepedia/UI and Images/Views/Child Views/PhoneImage.swift
@@ -29,7 +29,7 @@ struct PhoneImage: View {
 
     var body: some View {
 		#if os(iOS) || os(visionOS)
-		if let image = UIImage(data: phone.photoData ?? getPNGDataFromUIImage(image: .phone)) {
+		let image = UIImage(data: phone.photoData ?? getPNGDataFromUIImage(image: .phone))!
 			Image(uiImage: image)
                 .renderingMode(phone.photoData == nil ? .template : .original)
                 .resizable()
@@ -37,11 +37,8 @@ struct PhoneImage: View {
                 .frame(width: size, height: size)
                 .clipShape(RoundedRectangle(cornerRadius: SAContainerViewCornerRadius))
                 .accessibilityLabel("\(phone.brand) \(phone.model)")
-		} else {
-			PhoneImageUnavailableView()
-		}
 		#elseif os(macOS)
-		if let image = NSImage(data: phone.photoData ?? getPNGDataFromNSImage(image: .phone)) {
+		let image = NSImage(data: phone.photoData ?? getPNGDataFromNSImage(image: .phone))!
 			Image(nsImage: image)
                 .renderingMode(phone.photoData == nil ? .template : .original)
                 .resizable()
@@ -49,11 +46,9 @@ struct PhoneImage: View {
                 .frame(width: size, height: size)
                 .clipShape(RoundedRectangle(cornerRadius: SAContainerViewCornerRadius))
                 .accessibilityLabel("\(phone.brand) \(phone.model)")
-		} else {
-			PhoneImageUnavailableView()
-		}
 		#endif
     }
+
 }
 
 #Preview("Full") {
