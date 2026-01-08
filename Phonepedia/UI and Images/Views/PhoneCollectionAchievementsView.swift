d2f2a1e678dbfb195389c7b26f304cbae7b9ea8b
TechWithTyler <twsandmds@gmail.com>
2026-01-08 13:49:49 -0800

    Update PhoneCollectionAchievementsView.swift

--- Phonepedia/UI and Images/Views/PhoneCollectionAchievementsView.swift
+++ Phonepedia/UI and Images/Views/PhoneCollectionAchievementsView.swift
@@ -47,12 +47,15 @@ struct PhoneCollectionAchievementsView: View {
     var body: some View {
         NavigationStack {
             List {
+                PhoneCollectionAchievementRow(title: "Get A Corded And A Cordless Phone", condition: getCordedAndCordlessPhones)
                 PhoneCollectionAchievementRow(title: "Get A Brand-New Phone", condition: getABrandNewPhone)
                 PhoneCollectionAchievementRow(title: "Got A Phone In Its Release Year", condition: gotAPhoneInYearOfRelease)
                 PhoneCollectionAchievementRow(title: "Get 10 Phones", condition: phones.count >= 10)
                 PhoneCollectionAchievementRow(title: "Get 20 Phones", condition: phones.count >= 20)
                 PhoneCollectionAchievementRow(title: "Get 50 Phones", condition: phones.count >= 50)
                 PhoneCollectionAchievementRow(title: "Get 100 Phones", condition: phones.count >= 100)
+                PhoneCollectionAchievementRow(title: "Get 150 Phones", condition: phones.count >= 150)
+                PhoneCollectionAchievementRow(title: "Get 200 Phones", condition: phones.count >= 200)
             }
             .navigationTitle("Achievements")
             .toolbar {
