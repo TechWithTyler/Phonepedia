11577da698550df1260039edc58a40a603510e5c
TechWithTyler <twsandmds@gmail.com>
2026-01-08 13:50:47 -0800

    Update PhoneCollectionAchievementsView.swift

--- Phonepedia/UI and Images/Views/PhoneCollectionAchievementsView.swift
+++ Phonepedia/UI and Images/Views/PhoneCollectionAchievementsView.swift
@@ -49,7 +49,7 @@ struct PhoneCollectionAchievementsView: View {
             List {
                 PhoneCollectionAchievementRow(title: "Get A Corded And A Cordless Phone", condition: getCordedAndCordlessPhones)
                 PhoneCollectionAchievementRow(title: "Get A Brand-New Phone", condition: getABrandNewPhone)
-                PhoneCollectionAchievementRow(title: "Got A Phone In Its Release Year", condition: gotAPhoneInYearOfRelease)
+                PhoneCollectionAchievementRow(title: "Get A Phone In Its Release Year", condition: gotAPhoneInYearOfRelease)
                 PhoneCollectionAchievementRow(title: "Get 10 Phones", condition: phones.count >= 10)
                 PhoneCollectionAchievementRow(title: "Get 20 Phones", condition: phones.count >= 20)
                 PhoneCollectionAchievementRow(title: "Get 50 Phones", condition: phones.count >= 50)
