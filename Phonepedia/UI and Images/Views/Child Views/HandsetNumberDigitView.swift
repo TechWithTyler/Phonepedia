6128c2e6ba141b3e662fe812db8c7bcc7a651c47
TechWithTyler <twsandmds@gmail.com>
2026-04-24 13:58:55 -0700

    Update HandsetNumberDigitView.swift
    Fixed an issue where selecting the last dash or plus in a phone's model number could set the number of included cordless devices to more than 30.

--- Phonepedia/UI and Images/Views/Child Views/HandsetNumberDigitView.swift
+++ Phonepedia/UI and Images/Views/Child Views/HandsetNumberDigitView.swift
@@ -101,15 +101,17 @@ struct HandsetNumberDigitView: View {
     // MARK: - Set Handset Number Digit
 
     func setHandsetNumberDigit(to newDigit: Int, at index: Int) {
-        // 1. Set the number of included cordless handsets to the value corresponding to the new digit. For example, if the 2 after the dash in M123-2 is selected, the number of included cordless handsets will be set to 2. If the selected digit is 0, set it to 1.
+        // 1. If newDigit is -1, the last dash or plus in the model number was selected. Make sure the digits that follow (e.g. 1 and 2), joined as a String (e.g. 12) then converted to an Int, make up a number less than or equal to maxNumber.
         let newNumberOfIncludedCordlessHandsets: Int
         if newDigit == -1 {
+            let maxNumber = phone.handsetNumberDigitRepresents == 1 ? 29 : 30
+            guard let afterDash = phone.model.components(separatedBy: ["-", "+"]).last, let digits = Int(afterDash), digits <= maxNumber else { return }
             phone.handsetNumberDigitRepresents = modelNumberCharacters[index] == "+" ? 1 : 0
-            guard let afterDash = phone.model.components(separatedBy: ["-", "+"]).last, let digits = Int(afterDash) else { return }
             newNumberOfIncludedCordlessHandsets = digits == 0 ? 1 : digits
         } else {
             newNumberOfIncludedCordlessHandsets = newDigit == 0 ? 1 : newDigit
         }
+        // 2. Set the number of included cordless handsets to the value corresponding to the new digit. For example, if the 2 after the dash in M123-2 is selected, the number of included cordless handsets will be set to 2. If the selected digit is 0, set it to 1.
         phone.numberOfIncludedCordlessHandsets = phone.handsetNumberDigitRepresents == 1 ? newNumberOfIncludedCordlessHandsets + 1 : newNumberOfIncludedCordlessHandsets
         // 2. Set the handset number digit and index to the new digit and index.
         phone.handsetNumberDigit = newDigit
