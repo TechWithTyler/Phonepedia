197fdb0559d534e5e64a0b19a4743e8fa6a1ffb5
TechWithTyler <twsandmds@gmail.com>
2026-06-18 13:35:51 -0700

    Update PhoneCollectionAchievementTracker.swift
    Added achievements for getting n phones in a year.

--- Phonepedia/Model/Phone Collection Achievements/PhoneCollectionAchievementTracker.swift
+++ Phonepedia/Model/Phone Collection Achievements/PhoneCollectionAchievementTracker.swift
@@ -51,6 +51,25 @@ struct PhoneCollectionAchievementTracker {
         return phones.contains { $0.acquiredInYearOfRelease }
     }
 
+    // Years that phones were acquired in.
+    var acquisitionYears: [Int] {
+        // 1. Create an array of years.
+        var years: [Int] = []
+        // 2. For each phone in phones, add their acquisition years to the array.
+        for phone in phones {
+            let acquisitionYear = phone.acquisitionYear
+            let replacesPhoneAcquiredInYear = phone.replacesPhoneAcquiredInYear
+            if acquisitionYear > 0 {
+                years.append(acquisitionYear)
+            }
+            if replacesPhoneAcquiredInYear > 0 {
+                years.append(phone.replacesPhoneAcquiredInYear)
+            }
+        }
+        // 3. Return the array.
+        return years
+    }
+
     // Whether the phones array contains at least 1 phone with 2 or more cordless devices.
     var got2OrMoreCordlessDevices: Bool {
         return phones.contains { $0.cordlessHandsetsIHave.count >= 2 }
@@ -77,13 +96,13 @@ struct PhoneCollectionAchievementTracker {
     var all: [PhoneCollectionAchievement] {
         return [
             // Events
-            PhoneCollectionAchievement(id: "cordedAndCordless", title: "Get A Corded And A Cordless Phone", isUnlocked: hasCordedAndCordless),
-            PhoneCollectionAchievement(id: "brandNew", title: "Get A Brand-New Phone", isUnlocked: hasBrandNew),
-            PhoneCollectionAchievement(id: "releaseYear", title: "Get A Phone In Its Release Year", isUnlocked: gotInReleaseYear),
-            PhoneCollectionAchievement(id: "multiHandset", title: "Get 2 Or More Cordless Devices For A Phone", isUnlocked: got2OrMoreCordlessDevices),
-            PhoneCollectionAchievement(id: "callBlockPreScreening", title: "Get A Phone With Call Block Pre-Screening", isUnlocked: hasCallBlockPreScreening),
-            PhoneCollectionAchievement(id: "BTCellLinking", title: "Get A Phone With Bluetooth Cell Phone Linking", isUnlocked: hasBluetoothCellLinking),
-            PhoneCollectionAchievement(id: "placeOnBasePowerBackup", title: "Get A Cordless Phone With Place-On-Base Power Backup", isUnlocked: hasPlaceOnBasePowerBackup),
+            PhoneCollectionAchievement(id: "cordedAndCordless", title: "Get a Corded And a Cordless Phone", isUnlocked: hasCordedAndCordless),
+            PhoneCollectionAchievement(id: "brandNew", title: "Get a Brand-New Phone", isUnlocked: hasBrandNew),
+            PhoneCollectionAchievement(id: "releaseYear", title: "Get a Phone In Its Release Year", isUnlocked: gotInReleaseYear),
+            PhoneCollectionAchievement(id: "multiHandset", title: "Get 2 or More Cordless Devices for a Phone", isUnlocked: got2OrMoreCordlessDevices),
+            PhoneCollectionAchievement(id: "callBlockPreScreening", title: "Get a Phone With Call Block Pre-Screening", isUnlocked: hasCallBlockPreScreening),
+            PhoneCollectionAchievement(id: "BTCellLinking", title: "Get a Phone With Bluetooth Cell Phone Linking", isUnlocked: hasBluetoothCellLinking),
+            PhoneCollectionAchievement(id: "placeOnBasePowerBackup", title: "Get a Cordless Phone With Place-On-Base Power Backup", isUnlocked: hasPlaceOnBasePowerBackup),
             // Phone Counts
             PhoneCollectionAchievement(id: "count10Phones", title: "Get 10 Phones", isUnlocked: reachedPhoneCount(10)),
             PhoneCollectionAchievement(id: "count20Phones", title: "Get 20 Phones", isUnlocked: reachedPhoneCount(20)),
@@ -97,7 +116,9 @@ struct PhoneCollectionAchievementTracker {
             PhoneCollectionAchievement(id: "count50Handsets", title: "Get 50 Cordless Devices Across All Phones", isUnlocked: reachedCordlessDeviceCount(50)),
             PhoneCollectionAchievement(id: "count100Handsets", title: "Get 100 Cordless Devices Across All Phones", isUnlocked: reachedCordlessDeviceCount(100)),
             PhoneCollectionAchievement(id: "count150Handsets", title: "Get 150 Cordless Devices Across All Phones", isUnlocked: reachedCordlessDeviceCount(150)),
-            PhoneCollectionAchievement(id: "count200Handsets", title: "Get 200 Cordless Devices Across All Phones", isUnlocked: reachedCordlessDeviceCount(200))
+            PhoneCollectionAchievement(id: "count200Handsets", title: "Get 200 Cordless Devices Across All Phones", isUnlocked: reachedCordlessDeviceCount(200)),
+            PhoneCollectionAchievement(id: "acquired5PhonesInAYear", title: "Get 5 Phones In a Year", isUnlocked: reachedPhoneCountInAYear(5)),
+            PhoneCollectionAchievement(id: "acquired10PhonesInAYear", title: "Get 10 Phones In a Year", isUnlocked: reachedPhoneCountInAYear(10))
         ]
     }
 
@@ -106,6 +127,18 @@ struct PhoneCollectionAchievementTracker {
     // Returns whether the phones array contains at least n phones.
     func reachedPhoneCount(_ n: Int) -> Bool {
         return phones.count >= n
+    }
+
+    // Returns whether n phones were acquired in at least one particular year.
+    func reachedPhoneCountInAYear(_ n: Int) -> Bool {
+        // 1. Create a dictionary to store the year and count, where the key is the year and the value is the number of times the year exists in the array.
+        var yearCounts: [Int : Int] = [:]
+        // 2. For each occurrence of the acquisition year in the array, add 1 for that year in the dictionary.
+        for year in acquisitionYears {
+            yearCounts[year, default: 0] += 1
+        }
+        // 3. Return whether the dictionary's values contains any number greater than or equal to n.
+        return yearCounts.values.contains { $0 >= n }
     }
 
     // Returns whether the total number of cordless devices across all phones in the phones array is at least n.
